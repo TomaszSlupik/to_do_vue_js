1b80f54ddad7df18320c87aefca335ae36194d8c
Tomasz Słupik <Tomasz.Slupik@eurocash.pl>
2024-05-02 13:23:39 +0200

    to_do

---
+++ src/components/ToDo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>  
+    <button @click="clickMe">Kliknij</button>
+      <div 
+      v-for="item in items"
+      :key="item.id"
+      >
+        {{ item.title }}
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  props: {
+    msg: String
+  },
+  // Metoda na kliknięcie
+  methods: {
+    clickMe () {
+      alert('Kliknięto w przcisk')
+    }
+  },
+  // Moja lista zakupów
+  data () {
+    return {
+      items: [
+          {id: 0, title: 'zrobić zakupy'},
+          {id: 1, title: 'pójśc do sklepu'},
+      ]
+    }
+  }
+  
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
