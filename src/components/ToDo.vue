0fdcaeb6343de07058a0256e7a92a86875a09bb5
Tomasz Słupik <Tomasz.Slupik@eurocash.pl>
2024-05-03 16:31:21 +0200

    change

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -17,10 +17,15 @@
     >Dodaj</button>
 
       <div 
+      class="todoitem"
       v-for="item in items"
       :key="item.id"
       >
         {{ item.title }}
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+      </svg>
       </div>
   </div>
 </template>
@@ -66,8 +71,27 @@ button {
   cursor: pointer;
 }
 
+input {
+  height: 40px;
+}
+
 .todo {
   margin-right: 0.3em;
+}
+
+.todoitem {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.bi-trash {
+  color: red;
+}
+
+svg {
+  cursor: pointer;
 }
 
 </style>
